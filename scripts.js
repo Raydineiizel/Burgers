142944a49731ae3f7684d243ec945171a530fbe2
Raydineiizel <raydinei_izel@hotmail.com>
2026-06-22 23:08:25 -0400

    Commit Projeto Burger

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const list = document.querySelector('ul')
+const buttonShowAll = document.querySelector('.show-all')
+const buttonMapAll = document.querySelector('.map-all')
+let myLi = ''
+
+function showAll() {
+    menuOptions.forEach((product) => {
+        myLi += `
+    <li>
+        <img src=${product.src}>
+        <p>${product.name}</p>
+        <p class="item-price">R$ ${product.price}</p>
+    </li>
+  `
+
+    })
+
+    list.innerHTML = myLi
+}
+function mapAllItens() {
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9, // Dar 10% de desconto.
+    }))
+
+    //Spread Operatore
+    console.log(newPrices)
+}
+
+buttonShowAll.addEventListener('click', showAll)
+buttonMapAll.addEventListener('click', mapAllItens)
